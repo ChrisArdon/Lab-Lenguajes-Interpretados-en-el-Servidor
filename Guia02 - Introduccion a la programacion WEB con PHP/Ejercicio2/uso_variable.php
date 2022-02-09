29aad08041b52a837883a8642b2b1113835ac8c3
Chris Ardon <rockrisbl@gmail.com>
2022-02-09 14:12:27 -0600

    Ejercicio2 terminado

---
+++ Guia02 - Introduccion a la programacion WEB con PHP/Ejercicio2/uso_variable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Ejemplo de PHP</title>
+    <style>
+        body {
+            font-family: "Arial Black";
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    //Declaracion de variables
+    $a = 1;
+    $b = 3.34;
+    $c = "Hola mundo";
+    echo $a, "<br>", $b, "<br>", $c;
+    ?>
+</body>
+
+</html>
